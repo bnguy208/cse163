3a277f207d5ed246408e2cea40d6fbee0046fc4d
bnguy208 <bnguy208@uw.edu>
2023-08-12 20:14:21 -0700

    Updated wa_overdose_change( )

--- final_project.py
+++ final_project.py
@@ -26,8 +26,9 @@ def extract_xlsx(xlsx_name, ws_name):
 
 def merge_geo(shp_file_name, xlsx_file=None, csv_file_name=None):
     """
-    This function takes the name of the shapes file and Excel workbook file OR a CSV file
-    and returns a geospatial dataframe that joins these two datasets by state.
+    This function takes the name of the shapes file and Excel workbook file OR
+    a CSV file and returns a geospatial dataframe that joins these two datasets
+    by state.
     """
     shp_file = gpd.read_file(shp_file_name)
     if xlsx_file is None:
@@ -46,25 +47,36 @@ def merge_geo(shp_file_name, xlsx_file=None, csv_file_name=None):
 
 # (1) How has the number of drug overdose cases changed between 2015 and 2023
 # in Washington State?
-def drug_overdose_change(data, start=2015.0, end=2023.0) -> None:
+def wa_overdose_change(data, start=2016.0, end=2022.0) -> pd.DataFrame:
     """
     This function takes in the geospatial dataframe and returns the number
-    of drug overdose cases from 2015-2023.
+    of drug overdose cases in Washington from 2016-2023.
     """
-
-    # This chunk below is replaced by the overdose_geo() call but I'm too
-    # scared to delete it yet
-    '''
-    data = data[data["STATE_NAME"] == "Washington"]
-    drug = data["Drug Category"] == "Any Drug"
-    county = data["Geography"] == "County"
-    year = (data["Year"] >= start) & (data["Year"] <= end)
-    time = data["Time Aggregation"] == "1 year rolling counts"
-    remove_star = data["Death Count"] != "*"
-    county_data = data[drug & county & time & year & remove_star]
+    wa_data = data[data["STATE_NAME"] == "Washington"].copy()
+    drug = wa_data["Drug Category"] == "Any Drug"
+    county = wa_data["Geography"] == "County"
+    wa_data["Year"] = pd.to_numeric(data["Year"], errors="coerce")
+    year = (wa_data["Year"] >= start) & (wa_data["Year"] <= end)
+    time = wa_data["Time Aggregation"] == "1 year rolling counts"
+    remove_star = wa_data["Death Count"] != "*"
+    county_data = wa_data[drug & county & time & year & remove_star].copy()
     county_data["Death Count"] = county_data["Death Count"].astype("int")
-    county_data = county_data.dissolve(by="Year", aggfunc="sum").reset_index()
-    '''
+    county_data = \
+        county_data.groupby("Year").agg({"Death Count": "sum"}).reset_index()
+
+    fig = px.line(
+        county_data,
+        x="Year",
+        y="Death Count",
+        title=f"Drug Overdose Deaths in WA between "
+        f"{int(start)} and {int(end)}",
+        markers=True
+    )
+    fig.update_layout(title_x=0.5, title_y=0.95, font=dict(size=15))
+    fig.update_traces(line=dict(width=4), marker=dict(size=10))
+
+    # fig.show()
+    fig.write_image('wa_overdose.png')
 
 
 #  (2) Which counties in Washington state have the highest number/rate
@@ -101,34 +113,6 @@ def overdose_deaths_counties(data: gpd.GeoDataFrame, drug_name='Any Drug',
     plt.savefig('counties_overdose.png')
 
 
-def overdose_geo(data, start=2016.0, end=2022.0) -> pd.DataFrame:
-    wa_data = data[data["STATE_NAME"] == "Washington"].copy()
-    drug = wa_data["Drug Category"] == "Any Drug"
-    county = wa_data["Geography"] == "County"
-    wa_data["Year"] = pd.to_numeric(data["Year"], errors="coerce")
-    year = (wa_data["Year"] >= start) & (wa_data["Year"] <= end)
-    time = wa_data["Time Aggregation"] == "1 year rolling counts"
-    remove_star = wa_data["Death Count"] != "*"
-    county_data = wa_data[drug & county & time & year & remove_star].copy()
-    county_data["Death Count"] = county_data["Death Count"].astype("int")
-    county_data = \
-        county_data.groupby("Year").agg({"Death Count": "sum"}).reset_index()
-
-    fig = px.line(
-        county_data,
-        x="Year",
-        y="Death Count",
-        title=f"Drug Overdose Deaths in WA between "
-        f"{int(start)} and {int(end)}",
-        markers=True
-    )
-    fig.update_layout(title_x=0.5, title_y=0.95, font=dict(size=15))
-    fig.update_traces(line=dict(width=4), marker=dict(size=10))
-
-    # fig.show()
-    fig.write_image('wa_overdose.png')
-
-
 def overdose_df(geo_data: gpd.GeoDataFrame) -> pd.DataFrame:
     county_data_pd = pd.DataFrame(geo_data.drop(columns='geometry'))
     county_data_pd = county_data_pd.drop(columns='ALAND')
@@ -140,14 +124,15 @@ def overdose_df(geo_data: gpd.GeoDataFrame) -> pd.DataFrame:
 # (3) How does the number of overdoses in WA compare to number of overdoses in
 # other states in the USA? - Brandon
 def wa_versus_us(usa_data):
-    usa_data = usa_data[['State', 'Year', 'Month', 'Period', 'Indicator', 'Data Value']].copy()
+    usa_data = usa_data[['State', 'Year', 'Month', 'Period', 'Indicator', 
+                         'Data Value']].copy()
     usa_data['Year'] = usa_data['Year'].astype(str)
     any_drug = (usa_data['Indicator'] == 'Number of Drug Overdose Deaths')
 
     # Create masks
     year = (usa_data['Year'] == '2019') | (usa_data['Year'] == '2020')
     month = (usa_data['Month'] == 'March')
-    states = (usa_data['State'] == 'WA') | (usa_data['State'] == 'US') # State US?
+    states = (usa_data['State'] == 'WA') | (usa_data['State'] == 'US')
 
     # Filter data
     usa_data = usa_data[any_drug & year & month & states]
@@ -165,16 +150,17 @@ def wa_versus_us(usa_data):
 
 
 def main():
+    # Loading in the overdose data
     xlsx_file = extract_xlsx("Data/OverdoseDeathWA.xlsx",
                              "By Location and Date")
     wa_geo_data = merge_geo("Data/geodata/cb_2022_us_county_500k.shp",
                             xlsx_file)
     national_geo_data = merge_geo(
         "Data/geodata/cb_2022_us_county_500k.shp",
-        csv_file_name="Data/NationalOverdose.csv",
-    )
-    # drug_overdose_change(wa_geo_data)
-    overdose_geo(wa_geo_data)
+        csv_file_name="Data/NationalOverdose.csv")
+
+    # Methods to answer research questions
+    wa_overdose_change(wa_geo_data)
     overdose_deaths_counties(wa_geo_data)
     wa_versus_us(national_geo_data)
     # most_prevalent_drug(national_geo_data)
