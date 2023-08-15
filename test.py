0acb5fd7d865fb66ca97c69d48ae9c4af91e45a3
bnguy208 <bnguy208@uw.edu>
2023-08-14 21:21:50 -0700

    Corrected main method

--- test.py
+++ test.py
@@ -4,3 +4,36 @@ CSE 163 AA
 
 This file tests the methods in the script.
 """
+
+from extract_and_merge import extract_xlsx, merge_geo
+from question_1 import wa_overdose_change
+from question_2 import overdose_deaths_counties
+from question_3 import wa_versus_us
+
+
+def test_wa_overdose_change():
+    pass
+
+
+def test_wa_overdose_change():
+    pass
+
+
+def test_overdose_deaths_counties():
+    pass
+
+
+def test_wa_versus_us():
+    pass
+
+
+def race_death_wa():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
